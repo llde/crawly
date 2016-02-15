80a14d6b6d6a4fb0a853bdc18cc1283adaf7bf31
lorenzo <lorenzofersteam@live.it>
2016-02-15 18:20:32 +0100

    a todo

--- src/wsa/exceptions/VisitException.java
+++ src/wsa/exceptions/VisitException.java
@@ -6,7 +6,7 @@ import wsa.web.SiteCrawler;
  * Created by lorenzo on 2/12/16.
  */
 public class VisitException extends Exception {
-
+    //TODO Investigate I/O Permission denied
     public enum VisitState{NOT_RECOGNIZABLE, CORRUPTED}
     public VisitException(SiteCrawler sss, VisitState state) {
         super( state == VisitState.NOT_RECOGNIZABLE ?  "Non ho trovato una visita valida per : " : "LA visita sembra corrotta per: "
